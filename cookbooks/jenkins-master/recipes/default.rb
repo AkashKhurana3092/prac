dcfd04a25cc4947219368de182600c72834ec754
akashkhurana <akashkhurana3092@gmail.com>
2022-07-26 06:45:49 +0000

    Installing Kitchen Docker Gem

--- cookbooks/jenkins-master/recipes/default.rb
+++ cookbooks/jenkins-master/recipes/default.rb
@@ -6,21 +6,30 @@
 
 apt_update
 
-package node['java']['jdk'] do
+#package node['java']['jdk'] do
+#  action :install
+#end
+
+#remote_file "#{Chef::Config[:file_cache_path]}/jenkins.war" do
+#  source "https://get.jenkins.io/war-stable/#{node['jenkins']['version']}/jenkins.war"
+#  action :create
+#end
+
+#execute 'jenkins_command' do
+#  command "nohup java -jar #{Chef::Config[:file_cache_path]}/jenkins.war &"
+#end
+
+#execute 'show initial password' do
+#  command 'sudo cat /root/.jenkins/secrets/initialAdminPassword'
+#  live_stream true
+#  action :run
+#end
+
+package 'docker.io' do
   action :install
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/jenkins.war" do
-  source "https://get.jenkins.io/war-stable/#{node['jenkins']['version']}/jenkins.war"
-  action :create
+group 'docker' do
+  members ['ubuntu']
 end
 
-execute 'jenkins_command' do
-  command "nohup java -jar #{Chef::Config[:file_cache_path]}/jenkins.war &"
-end
-
-execute 'show initial password' do
-  command 'sudo cat /root/.jenkins/secrets/initialAdminPassword'
-  live_stream true
-#  action :run
-end
